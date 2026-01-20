24a2b3d15a03407380403a26275cb62c2c1c8f8b
Balram-Gupta <Balramgupta20101@gmail.com>
2026-01-20 10:36:02 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,19 @@
-  const express = require("express");
-    const router = express.Router();
+const express = require("express");
+const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-const { saveRedirect } = require("../middleware");
 const userController = require("../controller/user");
 
-  router.route("/signup")
- .get((req, res)=>{
-        res.render("./user/signup.ejs");
-    })   
-   .post(wrapAsync (userController.signup));
+router.post("/signup", wrapAsync(userController.signup));
 
-  
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureMessage: true
+  }),
+  userController.login
+);
 
-      router.route("/login")
-      .get((req, res)=>{
-        res.render("./user/login.ejs");
-    }).post(
-      saveRedirect,
-      passport.authenticate("local",
-         { failureRedirect: "/login", 
-            failureFlash : true,}), 
-      userController.login);
+router.post("/logout", userController.logout);
 
-
-      //logout route
-  router.get("/logout", userController.logout);
-
-    module.exports = router;
+module.exports = router;
